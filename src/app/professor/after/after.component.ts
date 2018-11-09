d35cd3778a6a191d9562861ade2bc50ac79e425d
Martina Lofqvist <34751844+MartinaLof@users.noreply.github.com>
2018-11-08 20:00:15 -0800

    Revert "Joyce"

--- src/app/professor/after/after.component.ts
+++ src/app/professor/after/after.component.ts
@@ -1,24 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
-import { Component, OnInit, HostListener } from '@angular/core';
-import { Router } from '@angular/router';
-import { AppComponent, Course } from '../../app.component';
 @Component({
   selector: 'app-after',
   templateUrl: './after.component.html',
   styleUrls: ['./after.component.css']
 })
 export class AfterComponent implements OnInit {
-  course: Course;
 
-  constructor(private router: Router, private app: AppComponent) { }
+  constructor() { }
 
   ngOnInit() {
-    this.course = this.app.currentCourse;
   }
-
-  onStart() {
-    this.router.navigateByUrl('/professor');
-  }
-
 
 }
