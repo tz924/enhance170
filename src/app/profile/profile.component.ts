60c00ef834ecc0befee21db6391048406029e49b
xijiao22 <xijiao@ucsd.edu>
2018-11-15 20:37:46 -0800

    yes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-<<<<<<< HEAD
-
-=======
-import { Router } from '@angular/router';
-import { CourseService } from 'src/app/course.service';
->>>>>>> 62fe4f42bac22d1e45a0f2a3ce6a8f6338773bbb
 
 @Component({
   selector: 'app-profile',
